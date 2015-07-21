d6443a325c31c0d489c1fd9c98453fb2d1097d7a
Jeff Ross-Ibarra <rossibarra@gmail.com>
2015-07-21 15:27:29 -0700

    second attempt at phase_mom bugfix

--- phasekids.R
+++ phasekids.R
@@ -131,12 +131,17 @@ phase_mom<-function(mom,progeny,win_length){
       } else if(same==(win_length-1) ){ #same phase as last window
         mom_phase1[length(mom_phase1)+1]=win_hap[length(win_hap)]
         mom_phase2[length(mom_phase2)+1]=1-win_hap[length(win_hap)]
-      } else{ #PHASE ERROR!
-        warning(paste("Likely recombination at",winstart,sep=" "))
-        bad_phase=ifelse(
-          sum(abs(mom_phase1[winstart:(winstart+(length(win_hap)-2))]-win_hap[1:length(win_hap)-1]))>sum(abs(mom_phase2[winstart:(winstart+(length(win_hap)-2))]-win_hap[1:length(win_hap)-1])),
-          mom_phase2,mom_phase1
-        ) #returns the minimum distance haplotype (prepare to screw up phase!)
+      } else{ ##returns the minimum distance haplotype (prepare to screw up phase!)
+          warning(paste("Likely recombination at",winstart,sep=" "))
+          diff1=sum(abs(mom_phase1[winstart:(winstart+(length(win_hap)-2))]-win_hap[1:length(win_hap)-1]))
+          diff2=sum(abs(mom_phase2[winstart:(winstart+(length(win_hap)-2))]-win_hap[1:length(win_hap)-1]))
+          if(diff1>diff2){ #momphase1 is less similar to current inferred hap
+            mom_phase2[length(mom_phase2)+1]=win_hap[length(win_hap)]
+            mom_phase1[length(mom_phase1)+1]=1-win_hap[length(win_hap)]
+          } else{ #momphase1 is more similar
+            mom_phase1[length(mom_phase1)+1]=win_hap[length(win_hap)]
+            mom_phase2[length(mom_phase2)+1]=1-win_hap[length(win_hap)]
+          }
       }
     }
   }
