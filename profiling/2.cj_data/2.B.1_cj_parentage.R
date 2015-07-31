2ffaa4ddb5e3baec238c8ef4936afd3942596a88
yangjl0930@gmail.com <yangjl0930@gmail.com>
2015-07-30 18:31:45 -0700

    updated parentage.

--- profiling/2.cj_data/2.B.1_cj_parentage.R
+++ profiling/2.cj_data/2.B.1_cj_parentage.R
@@ -2,46 +2,6 @@
 # You'll need this amount of memory to open the parentage.Rdata file
 # srun -p bigmemh --mem 64000 --pty R
 
-#setwd("/group/jrigrp4/phasing/cj_teosinte")
-
-ob <- load("/group/jrigrp4/phasing/cj_teosinte/parentage.Rdata")
-
-#need the ProgenyArray, ignore
-#str(pa)
-
-# parents/kids index
-parent.names <- colnames(pa@parents_geno)
-progeny.names <- colnames(pa@progeny_geno)
-
-
-x <- rep(1:70, 1)
-
-parents <- data.frame(parent.names,x)
-
-# index file
-
-idx.all <- data.frame(pa@parents)
-progeny.names <- colnames(pa@progeny_geno)
-
-idx.all <- data.frame(progeny.names, idx.all)
-
-idx.all <- idx.all[,1:4]
-
-# Merge
-
-new.df <- merge(idx.all, parents, by.x="parent_1", by.y="x")
-colnames(new.df)[colnames(new.df)=="parent.names"] <- "parent1_names"
-
-df <- merge(new.df, parents, by.x="parent_2", by.y="x")
-colnames(df)[colnames(df)=="parent.names"] <- "parent2_names"
-
-final.df <- data.frame(df$progeny.names, df$parent1_names, df$parent2_names)
-colnames(final.df) <- c("progeny", "parent1", "parent2")
-write.table(final.df, "parents.txt")
-
-# Now exit out and open a new session
-
-rm(list=ls())
 
 #library(plyr)
 ped <- read.table("/group/jrigrp4/phasing/cj_teosinte/parents.txt", header =TRUE)
@@ -60,35 +20,18 @@ oxinfo <- data.frame(table(c(ox$parent1, ox$parent2)))
 names(oxinfo) <- c("founder", "nox")
 
 pinfo <- merge(pinfo, oxinfo, by="founder", all=TRUE)
+pinfo$sid <- gsub("_1.*", "", pinfo$founder)
+pinfo$sid <- gsub("_mrg.*", "", pinfo$sid)
+
+wgs <- read.csv("data/wgs_teo19_id.csv")
+
+
+pinfo <- merge(pinfo, wgs, by.x="sid", by.y="id", all=TRUE)
+
+sub <- subset(pinfo, !is.na(WGS))
 
 
 
-fams[]<- lapply(fams, as.character)
-x <- which(fams$parent1==fams$parent2)
-
-new.df <- fams[row.names(fams) %in% x,]
-
-new.df[] <- lapply(new.df, as.factor)
 
 
-# These below should be identical for the selves
-parent1 <- table(new.df$parent1)
-
-sort(parent1)
-
-parent2 <- table(new.df$parent2)
-
-sort(parent2)
-
-
-# Now get those familes, and make a list of them to retrieve
-biggest <- subset(new.df, parent1=="PC_O51_ID2_mrg:250276282")
-smallest <- subset(new.df, parent1=="PC_J10_ID1_2:250276214")
-
-biggest.array.kids <- biggest[,1]
-smallest.array.kids <- smallest[,1]
-
-#Append the parent name
-write.table(biggest.array.kids,"biggest_selfed_family.txt")
-write.table(smallest.array.kids, "smallest_selfed_family.txt")
 
