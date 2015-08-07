bea29b4e46e0fe066ab8e9863ec13075389fbeff
yangjl0930@gmail.com <yangjl0930@gmail.com>
2015-08-07 14:33:53 -0700

    need to fix the error message

--- profiling/2.cj_data/2.C.1_cj_phasing.R
+++ profiling/2.cj_data/2.C.1_cj_phasing.R
@@ -7,6 +7,65 @@ FormatData(wgs, cols=9:27)
 
 
 
+ob <- load("largedata/sfamdata/PC_I11_ID2.RData")
+
+### loading all the functions in folder "lib"
+f <- sapply(list.files(pattern="[.]R$", path="lib", full.names=TRUE), source)
+
+
+### simulation perfect mom and noisy kids
+#set.seed(1235678*i)
+sim <- SimSelfer(size.array=10, het.error=0.5, hom.error=0.02, numloci=40000, rec=1.5, imiss=0.3)
+#plotselfer(sim, kids=6:10, snps=40:100, cols=c("green", "blue"))
+
+
+### format to phase_mom
+input <- sim2input(sim)
+probs <- get_error_mat(0.02, 0.5)[[2]]
+#estimated_mom <- input[[1]]
+progeny <- input[[2]]
+
+#MOM PHASE
+
+
+win_length=10
+verbose=TRUE
+for(i in 1:10){
+    chr <- subset(mk, chr==i)
+    
+    progeny <- list(list())
+    for(j in 6:ncol(chr)){
+        progeny[[i]][[1]] <- chr[, j]
+        progeny[[i]][[2]] <- chr[, j]
+    }
+    
+    newmom <- phasing(estimated_mom=chr[, 5], progeny, win_length, verbose=TRUE)
+    #plotphasing(sim, kids=1:5, snps=1:1000, cols=c("red", "blue"), plotphasing=TRUE, newmom)
+}
+
+#1: phasing(estimated_mom = chr[, 5], progeny, win_length, verbose = TRUE)
+#2: PhaseMom.R#9: phase_mom_chuck(estimated_mom, progeny, win_length, verbose, 
+#3: PhaseMom.R#111: jump_win(winstart, win_length, hetsites, mom_haps)
+#4: PhaseMom.R#160: infer_dip(momwin, progeny, haps = mom_haps, returnhap = FAL
+#5: PhaseMom.R#202: sapply(1:(length(haps)), function(a) runoverhaps(a))
+#6: sapply(1:(length(haps)), function(a) runoverhaps(a))
+#7: lapply(X = X, FUN = FUN, ...)
+#8: PhaseMom.R#202: FUN(X[[i]], ...)
+#9: PhaseMom.R#202: runoverhaps(a)
+#10: PhaseMom.R#199: sapply(1:length(progeny), function(z) which_phase(haps[myha
+#===>  11: sapply(1:length(progeny), function(z) which_phase(haps[myhap], progeny[[z]]
+#12: lapply(X = X, FUN = FUN, ...)
+#13: FUN(X[[i]], ...)
+# 14: PhaseMom.R#199: which_phase(haps[myhap], progeny[[z]][[2]][momwin])
+                                                      
+                                                      
+
+
+
+
+
+
+
 #######################################
 FormatData <- function(wgs, cols){
     
