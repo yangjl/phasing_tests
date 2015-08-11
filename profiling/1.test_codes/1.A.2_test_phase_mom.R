b57585deae6568715c25e2068f1a0be334c5d3f0
yangjl0930@gmail.com <yangjl0930@gmail.com>
2015-08-11 16:41:25 -0700

    minor changes

--- profiling/1.test_codes/1.A.2_test_phase_mom.R
+++ profiling/1.test_codes/1.A.2_test_phase_mom.R
@@ -20,7 +20,8 @@ sourceall(rm=TRUE)
 for(i in 1:1){
     ### simulation perfect mom and noisy kids
     #set.seed(1235678*i)
-    sim <- SimSelfer(size.array=10, het.error=0.7, hom.error=0.002, numloci=40000, rec=1.5, imiss=0.3)
+    #sim <- SimSelfer(size.array=10, het.error=0.7, hom.error=0.002, numloci=40000, rec=1.5, imiss=0.3)
+    sim <- SimSelfer(size.array=10, het.error=0.5, hom.error=0.02, numloci=40000, rec=1.5, imiss=0.3)
     #plotselfer(sim, kids=6:10, snps=40:100, cols=c("green", "blue"))
     
     
