cd707defdfe500f42b13430fad91565bbbc2c238
yangjl0930@gmail.com <yangjl0930@gmail.com>
2015-08-12 13:19:36 -0700

    more tests

--- profiling/1.test_codes/1.A.2_test_phase_mom.R
+++ profiling/1.test_codes/1.A.2_test_phase_mom.R
@@ -18,7 +18,7 @@ sourceall(rm=TRUE)
 
 #######################################################################################################
 ### simulation perfect mom and noisy kids
-#set.seed(1235678*i)
+set.seed(3*as.numeric(as.character(job)))
 #sim <- SimSelfer(size.array=10, het.error=0.7, hom.error=0.002, numloci=40000, rec=1.5, imiss=0.3)
 sim <- SimSelfer(size.array=10, het.error=0.5, hom.error=0.02, numloci=1000, rec=1.5, imiss=0.3)
 #plotselfer(sim, kids=6:10, snps=40:100, cols=c("green", "blue"))
--- profiling/1.test_codes/1.A.3_test_error_rate.R
+++ profiling/1.test_codes/1.A.3_test_error_rate.R
@@ -2,14 +2,15 @@
 ### July 30th, 2015
 
 
-files <- list.files(path="slurm", pattern="txt$")
+files <- list.files(path="largedata/out/", pattern="txt$")
 
-err <- data.frame()
+out <- data.frame()
 for(i in 1:length(files)){
-    out <- read.table(paste0("test/", files[i]), header=TRUE)
-    err <- rbind(err, out)
+    tem <- read.table(paste0("largedata/out/", files[i]), header=TRUE)
+    tem$file <- files[i]
+    out <- rbind(out, tem)
 }
 
-err$er <- err$diff/err$tot
+err$er <- err$err/err$tot
 
 
