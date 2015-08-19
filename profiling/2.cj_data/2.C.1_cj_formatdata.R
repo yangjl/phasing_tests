864e15d157b550bc65609152beb8587c116c6843
yangjl0930@gmail.com <yangjl0930@gmail.com>
2015-08-19 16:46:04 -0700

    start to phasing real data.

--- profiling/2.cj_data/2.C.1_cj_formatdata.R
+++ profiling/2.cj_data/2.C.1_cj_formatdata.R
@@ -46,4 +46,8 @@ FormatData <- function(wgs, cols){
 ### write PLINK format file
 FormatData(wgs, cols=9:27)
 
-
+files <- list.files(pattern="RData", path="largedata/sfamdata", full.names=TRUE)
+tem <- data.frame(input=rep(files, each=10), chr=rep(1:10, times=19), output=0)
+tem$output <- gsub(".RData", "_chr", tem$input)
+tem$output <- paste0(tem$output, tem$chr, ".RData")
+write.table(tem, "largedata/sfamdata/all_files.txt", sep="\t", row.names=FALSE, quote=FALSE)
---
+++ slurm-script/run_phasing.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+#SBATCH -D /home/jolyang/Documents/Github/phasing_tests
+#SBATCH -J phase_mom
+#SBATCH -o /home/jolyang/Documents/Github/phasing_tests/slurm/phase_out-%j.txt
+#SBATCH -e /home/jolyang/Documents/Github/phasing_tests/slurm/phase_error-%j.txt
+
+
+#$1: 1-160; row number of the all_files.txt
+R --no-save "--args $1 $SLURM_JOB_ID" < profiling/2.cj_data/2.C.2_phasing.R
+
+#usage: sbatch --p serial slurm/testrun.sh 1
