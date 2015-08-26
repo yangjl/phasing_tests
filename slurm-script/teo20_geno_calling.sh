7f76a5005daad45b52851cd9e3dcc710a9f2fa86
yangjl <jolyang@ucdavis.edu>
2015-08-26 15:46:14 -0700

    callsnp

--- slurm-script/teo20_geno_calling.sh
+++ slurm-script/teo20_geno_calling.sh
@@ -2,11 +2,12 @@
 #SBATCH -D /home/jolyang/Documents/Github/phasing_tests
 #SBATCH -o /home/jolyang/Documents/Github/phasing_tests/slurm-log/testout-%j.txt
 #SBATCH -e /home/jolyang/Documents/Github/phasing_tests/slurm-log/err-%j.txt
-#SBATCH -J align_test
+#SBATCH -J callsnps
 #SBATCH--mail-user=yangjl0930@gmail.com
 #SBATCH--mail-type=END
 #SBATCH--mail-type=FAIL #email if fails
 set -e
 set -u
 
-angsd -nThreads 10 -doMajorMinor 3 -sites largedata/genotype_calls/head10_gbs_sites_v2.txt -GL 2 -bam largedata/genotype_calls/teo20_bamfiles_v2.txt -doGeno 5 -doMaf 1 -doPost 1 -out largedata/genotype_calls/geno_call
+angsd sites index gbs_sites_v2.txt
+angsd -nThreads 20 -doMajorMinor 3 -sites largedata/genotype_calls/gbs_sites_v2.txt -GL 2 -bam largedata/genotype_calls/teo20_bamfiles_v2.txt -doGeno 5 -doMaf 1 -doPost 1 -out largedata/genotype_calls/geno_call
